2e973bb5d5b66a91ad29302212e3b63f02f82bef
Vadim Kim <vkim0203@gmail.com>
2024-11-16 14:38:23 +0900

    deployment failure ver 1

--- lib/data.ts
+++ lib/data.ts
@@ -2,8 +2,8 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/corpcomment.png";
-import rmtdevImg from "@/public/rmtdev.png";
+import corpcommentImg from "/public/corpcomment.png";
+import rmtdevImg from "@public/rmtdev.png";
 
 export const links = [
   {
