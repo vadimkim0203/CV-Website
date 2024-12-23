3aad246b9a1b2ff09bee799f425fd3c9a0a51d0c
Vadim Kim <vkim0203@gmail.com>
2024-12-23 12:10:20 +0900

    Tictac pj and investcalc pj added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import '././styles/globals.css';
+import './styles/globals.css';
 import { Inter } from 'next/font/google';
 import Header from '@/components/header';
 import ActiveSectionContextProvider from '@/context/active-section-context';
