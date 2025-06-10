e871655ca7a6293f914e4de13a2f5dd796886902
Vadim Kim <vkim0203@gmail.com>
2025-06-10 23:13:21 +0900

    feat:localization with i18n

--- components/about.tsx
+++ components/about.tsx
@@ -1,43 +1,62 @@
-"use client"
+'use client';
 
-import React from 'react'
-import SectionHeading from './section-heading'
-import { motion } from "framer-motion"
-import { useSectionInView } from '@/lib/hooks'
+import React from 'react';
+import SectionHeading from './section-heading';
+import { motion } from 'framer-motion';
+import { useSectionInView } from '@/lib/hooks';
+import { Switch } from './ui/switch';
+import { Label } from './ui/label';
 
 export default function About() {
-
-  const { ref } = useSectionInView("About");
-
+  const { ref } = useSectionInView('About');
 
   return (
     <motion.section
       ref={ref}
-      className='mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28'
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
-      id='about'>
+      id="about"
+    >
       <SectionHeading>About Me</SectionHeading>
+      <div className="flex items-center space-x-2">
+        <Switch id="airplane-mode" />
+        <Label htmlFor="airplane-mode">Airplane Mode</Label>
+      </div>
       <p>
-        After graduating with a degree in{" "}
-        <span className="font-medium">Political science</span>, I spent 7 years at Uniqlo, working my way up to{" "}
-        <span className="font-bold">store manager</span>. Later, I moved into the{" "}
-        <span className="font-medium">Korean skincare industry as a sales manager</span>, where I discovered an interest in web development. I{" "}
-        <span className="italic">started learning to code</span> and completed multiple free courses and bootcamps.{" "}
-        <span className="italic">My favorite part of programming</span> is the problem-solving aspect. I{" "}
-        <span className="underline">love</span> the feeling of finally figuring out a solution to a problem. My core stack includes{" "}
-        <span className="font-medium">TypeScript, TailwindCSS and Docker </span>. I am also familiar with NextJS and Supabase, and I am always looking to learn new technologies.{" "}
-        <span className="font-medium">I am currently seeking a full-time position as a frontend developer</span> to continue building my skills and experience.
+        After graduating with a degree in{' '}
+        <span className="font-medium">Political science</span>, I spent 7 years
+        at Uniqlo, working my way up to{' '}
+        <span className="font-bold">store manager</span>. Later, I moved into
+        the{' '}
+        <span className="font-medium">
+          Korean skincare industry as a sales manager
+        </span>
+        , where I discovered an interest in web development. I{' '}
+        <span className="italic">started learning to code</span> and completed
+        multiple free courses and bootcamps.{' '}
+        <span className="italic">My favorite part of programming</span> is the
+        problem-solving aspect. I <span className="underline">love</span> the
+        feeling of finally figuring out a solution to a problem. My core stack
+        includes{' '}
+        <span className="font-medium">TypeScript, TailwindCSS and Docker </span>
+        . I am also familiar with NextJS and Supabase, and I am always looking
+        to learn new technologies.{' '}
+        <span className="font-medium">
+          I am currently seeking a full-time position as a frontend developer
+        </span>{' '}
+        to continue building my skills and experience.
       </p>
 
-
-
       <p>
-        <span className="italic">When I'm not coding</span>, I love listening to music 🎼, traveling the world 🌎, and exploring new culinary concepts 🍣. I also enjoy{" "}
-        <span className="font-medium">learning new things</span> and am currently diving into{" "}
+        <span className="italic">When I'm not coding</span>, I love listening to
+        music 🎼, traveling the world 🌎, and exploring new culinary concepts
+        🍣. I also enjoy{' '}
+        <span className="font-medium">learning new things</span> and am
+        currently diving into{' '}
         <span className="font-medium">fashion and design</span>.
       </p>
     </motion.section>
-  )
+  );
 }
---
+++ src/i18n/routing.ts
@@ -0,0 +1,6 @@
+import { defineRouting } from 'next-intl/routing';
+
+export const locales = ['en', 'kr'];
+export const defaultLocale = 'en';
+
+export const routing = { locales, defaultLocale };
