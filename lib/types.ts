96e09aa460da0121724ebf55fc9ed38b3a13bcf2
Vadim Kim <vkim0203@gmail.com>
2025-06-17 22:44:36 +0900

    feat: add data types

--- lib/types.ts
+++ lib/types.ts
@@ -1,3 +1,10 @@
 import { links } from "./data";
 
 export type SectionName = (typeof links)[number]["name"];
+
+export interface ProjectData {
+    title: string, 
+    location: string, 
+    description: string,
+    
+}
